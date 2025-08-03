31a499d46a5a2ddf01c1c21d63a69f516951468d
ChamathNad <chamath.nad@gmail.com>
2025-08-03 13:39:06 +0530

    Service corousal fix

--- components/HomePage/ServiceHome.tsx
+++ components/HomePage/ServiceHome.tsx
@@ -6,7 +6,7 @@ export default function ServicesPage()
 {       
 
     return (
-        <div className='mx-auto w-full pt-2 duration-200 lg:max-w-[1500px] lg:h-[1000px] '>
+        <div className='mx-auto w-full pt-[2%] duration-200 lg:max-w-[1500px] lg:h-[1000px] '>
         <div className={"w-full flex flex-col justify-center group bg-gray-800 rounded-[25px] "}>
 
           <div className='w-full h-[20%] flex'>
@@ -16,9 +16,9 @@ export default function ServicesPage()
               <div className="absolute inset-0 z-2 bg-[url('/Images/BG_Circut.jpg')] bg-center bg-repeat bg-[length:1500px] mix-blend-plus-lighter blur-50"/>              
             </div>
 
-            <div className='mx-auto py-[3%] not-xl:w-[100vh] flex justify-between px-[5%] lg:px-[15%] z-5 font-family-Lufga text-white duration-200'>
-              <span className='flex-3 text-[25px] md:text-[40px] lg:text-[50px] '>What I <span className='text-Primary-500'>Build</span>...</span>
-              <span className='flex-5 text-[11px] md:text-[15px] lg:text-[20px]'>“Building immersive worlds in Unity and Unreal, crafting intuitive UIs in React, or prototyping with robotics and AR, offer versatile, full-stack solutions tailored for tomorrow.”</span>
+            <div className='mx-auto py-[5%] not-xl:w-[100vh] flex justify-between px-[10%] lg:px-[15%] z-5 font-family-Lufga text-white duration-200'>
+              <span className='flex-3 text-[24px] md:text-[35px] lg:text-[50px] '>What I <span className='text-Primary-500'>Build</span>...</span>
+              <span className='flex-6 text-[12px] md:text-[15px] lg:text-[20px]'>“Building immersive worlds in Unity and Unreal, crafting intuitive UIs in React, or prototyping with robotics and AR, offer versatile, full-stack solutions tailored for tomorrow.”</span>
             </div>
           </div>
           <div className='w-full h-[50%] flex justify-center '>
